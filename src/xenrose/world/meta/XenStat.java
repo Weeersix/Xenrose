3cfe6f5466ac71f65e5b1ea9b854c2529de29fba
Weeersix <lazerkruser132@gmail.com>
2025-06-27 17:39:47 +0300

    Add files via upload

---
+++ src/xenrose/world/meta/XenStat.java
@@ -0,0 +1,14 @@
+package xenrose.world.meta;
+
+import arc.*;
+import arc.struct.*;
+import mindustry.world.meta.Stat;
+import mindustry.world.meta.StatCat;
+
+import java.util.*;
+
+public class XenStat{
+
+    public static final Stat
+            powerTier = new Stat("powerTier", StatCat.function);
+}
