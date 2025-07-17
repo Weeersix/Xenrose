a2a91e589aa39731338bcc492a8036f25f117ae8
Weeersix <lazerkruser132@gmail.com>
2025-07-17 18:00:11 +0300

    Update XenLiquids.java

--- src/xenrose/XenContent/XenLiquids.java
+++ src/xenrose/XenContent/XenLiquids.java
@@ -12,15 +12,15 @@ public class XenLiquids {
             viscosity = 0.5f;
             lightColor = Color.valueOf("bd8cec").a(0.5f);
         }};
+        oxygen = new Liquid("oxygen", Color.valueOf("9097a1")){{
+            gas = true;
+            flammability = 3.4f;
+        }};
         liquidOrinil = new Liquid("liquid-orinil", Color.valueOf("ffc77e")){{
             temperature = 6f;
             viscosity = 0.35f;
             heatCapacity = 1.35f;
             lightColor = Color.valueOf("ffc77e").a(0.9f);
-        }};
-        oxygen = new Liquid("oxygen", Color.valueOf("9097a1")){{
-            gas = true;
-            flammability = 3.4f;
         }};
     }
 }
