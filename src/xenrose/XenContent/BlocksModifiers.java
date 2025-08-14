926f3f239c7602bd4cbcd463e8399ca2cbd030ca
Weeersix <lazerkruser132@gmail.com>
2025-08-14 16:56:31 +0300

    some fixes

--- src/xenrose/XenContent/BlocksModifiers.java
+++ src/xenrose/XenContent/BlocksModifiers.java
@@ -11,12 +11,12 @@ public class BlocksModifiers {
 
     public static void load(){
 
-                //Walls
-                diocasiumArmor = new Modifier("diocasium-armor"){{
-                    researchCost = ItemStack.with(XenItems.damascus, 1f);
-                }};
-                healTechTree = new SecondModifier("heal-tech-tree"){{
-                    researchCost = ItemStack.with(XenItems.damascus, 1f);
-                }};
-            }
+        //Walls
+        healTechTree = new SecondModifier("heal-tech-tree"){{
+            researchCost = ItemStack.with(XenItems.damascus, 8850, XenItems.zinc, 8490, XenItems.dantstalin, 6200, XenItems.gold, 5680, XenItems.protexide, 6740);
+        }};
+        diocasiumArmor = new Modifier("diocasium-armor"){{
+            researchCost = ItemStack.with(XenItems.damascus, 18960, XenItems.zinc, 16770, XenItems.dantstalin, 11430, XenItems.gold, 9850, XenItems.protexide, 10860, XenItems.diocasium, 5640);
+        }};
+    }
 }
